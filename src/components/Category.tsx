59e32f43bf6409b5459fbb9bfa409f1eddde32e5
muslimbeko8 <blakmaxmudov@gmail.com>
2025-01-28 16:46:52 +0500

    fixx

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -81,7 +81,7 @@ export default function Category() {
         <CategorySwipper />
         <div
           className="grid gap-6 mt-6 mx-auto w-full 
-          grid-cols-3
+          grid-cols-3 
           xl:grid-cols-3 
           lg:grid-cols-2 
           md:grid-cols-2 
